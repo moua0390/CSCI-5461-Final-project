983ad079fcb4ec8c6680f61d0b074b72b0cb6636
Hmong Moua <moua0390@umn.edu>
2024-04-25 16:33:27 -0500

    Group code into a function

--- phase1.R
+++ phase1.R
@@ -1,11 +1,21 @@
+# Unsupervised learning classifier — K-Means clustering
+
 install.packages("useful")
 library(useful)
 
-count_df <- read.csv("Integrated_scaled_counts_release.txt", header=TRUE) 
-
 set.seed(54612024)
-num_clusters = 30
 
-kmeans_result <- kmeans(count_df[-1, -1], num_clusters)
-plot(kmeans_result)
-print(kmeans_result)
+unsupervised_snc_classifier <- function(count_df) {
+  #count_df <- read.csv("Integrated_scaled_counts_release.txt", header=TRUE) 
+
+  num_clusters = 30
+
+  kmeans_result <- kmeans(count_df[-1, -1], num_clusters)
+  plot(kmeans_result)
+  print(kmeans_result)
+
+  # Append scores to the data set
+  scored_df <- cbind(count_df, kmeans_result)
+  
+  return (scored_df)
+}
