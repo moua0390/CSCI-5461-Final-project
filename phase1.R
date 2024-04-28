98a38ac5b96d2b74392849992d789b023ca19d42
roge0511 <167589265+roge0511@users.noreply.github.com>
2024-04-27 19:51:26 -0500

    added code for hierarchical, agnes, and PCA clustering

--- phase1.R
+++ phase1.R
@@ -9,3 +9,13 @@ num_clusters = 30
 kmeans_result <- kmeans(count_df[-1, -1], num_clusters)
 plot(kmeans_result)
 print(kmeans_result)
+
+# hierarchical clustering
+hierarchical_result <- hclust(dist(count_df))
+plot(hierarchical_result)
+# agglomerative nesting
+agnes_result <- agnes(count_df)
+plot(agnes_result, which.plots=2)
+# PCA
+pca_result <- prcomp(count_df, scale. = FALSE)
+# ADD CODE TO PLOT HERE
