833288fed9e73ae07cc0b573ea3a5f9ddeaaf4f9
Hmong Moua <moua0390@umn.edu>
2024-05-02 09:11:51 -0500

    Generate PCA plot for supervised classifier

--- runClassifiers.R
+++ runClassifiers.R
@@ -6,6 +6,11 @@ if (!require("vroom")) {
   library(vroom)
 }
 
+if (!require("ggfortify")) {
+  install.packages("ggfortify")
+  library(ggfortify)
+}
+
 # Read data
 integrated_df <- vroom("Integrated_scaled_counts_release.txt")
 meta_df <- vroom("Metadata_release.txt")
@@ -21,3 +26,14 @@ train <- integrated_df[integrated_df$...1 %in% unlist(unsupervised_scores[1]),]
 test <- heldout_df[heldout_df$...1 %in% unlist(unsupervised_scores[1]),]
 train_scores <- unlist(unsupervised_scores[2])
 supervised_scores <- supervised_snc_classifier(train, test, train_scores)
+
+# Create PCA plot of supervised results
+supervised_pca_result <- prcomp(t(test[,-1]), scale.=FALSE)
+supervised_plot <- data.frame(score=as.numeric(supervised_scores["score",]))
+autoplot(supervised_pca_result,
+         scale=0,
+         data=supervised_plot,
+         color="score") +
+  scale_colour_gradient(low="blue", high="red", "Score") +
+  ggtitle("2D PCA plot from Supervised SnC classifier scores") +
+  theme(plot.title=element_text(hjust = 0.5))
